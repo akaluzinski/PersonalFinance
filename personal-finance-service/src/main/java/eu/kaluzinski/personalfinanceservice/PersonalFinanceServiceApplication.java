e9b39a224f07559e45068a74816b79d9b3dbb59c
Adrian Kaluzinski <kaluza@gmail.com>
2023-03-30 15:31:04 +0200

    Set default timezone and change server port

--- personal-finance-service/src/main/java/eu/kaluzinski/personalfinanceservice/PersonalFinanceServiceApplication.java
+++ personal-finance-service/src/main/java/eu/kaluzinski/personalfinanceservice/PersonalFinanceServiceApplication.java
@@ -1,10 +1,18 @@
 package eu.kaluzinski.personalfinanceservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PersonalFinanceServiceApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalFinanceServiceApplication.class, args);
